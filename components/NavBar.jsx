0223d4a2ac3cbd8f88863a37d4582669d550a432
Abhishek <abhishekparmar162@gmail.com>
2022-10-26 22:33:26 +0530

    mobile responsive navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -12,20 +12,22 @@ const NavBar = () => {
         <div className='h-[50px] w-[50px] cursor-pointer'><HeadlessUI className='h-[20px] w-[20px]' /></div>
       </div>
       {/* Center  */}
-      <div className='flex w-[960px] h-[50px] '>
+      <div className='hidden  lg:flex w-[960px] h-[50px] '>
         <form className='flex ml-64 mt-1'>
           <input type="text" placeholder='Search' className='w-[355px] border border-gray-900 h-[36px]  bg-[#E5E5E5]'/>
           <FiSearch  size={20} className='bg-[#E5E5E5] h-[36px] '/>
         </form>
          </div>
          {/* Right  */}
-          <div className='h-[50px] w-[181px] flex  justify-between'>
+          <div className=' h-[50px] w-[181px] flex  justify-between'>
 
           <button className='cursor-pointer bg-gray-300    hover:bg-gray-400 h-[30px] w-[58px]'>Login</button>
           <button className='cursor-pointer h-[30px] w-[68px] hover:bg-[#772ce8] bg-[#5c16c5] text-gray-200'>SignUp</button>
           <button className='w-[30px] h-[30px]'><FiUser size={25} /></button>
           </div>
-         
+          {/* Mobile  */}
+
+          
     </div>
   )
 }
