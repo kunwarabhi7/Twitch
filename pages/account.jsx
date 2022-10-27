56c9ebd36c5e0718ebf1af91db5ce7116130bcea
Abhishek <abhishekparmar162@gmail.com>
2022-10-27 14:18:40 +0530

    added google auth

--- pages/account.jsx
+++ pages/account.jsx
@@ -1,21 +1,21 @@
 import { useSession, signIn, signOut } from "next-auth/react"
-
-const account = () => {
-    const { data: session } = useSession()
-    if (session) {
-      return (
-        <>
-          Signed in as {session.user.email} <br />
-          <button onClick={() => signOut()}>Sign out</button>
-        </>
-      )
-    }
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    console.log(session)
     return (
-      <>
-        Not signed in <br />
-        <button onClick={() => signIn()}>Sign in</button>
-      </>
+      <div className="w-full h-24 bg-black text-white">
+<p className=" text-xl w-48 h-12 pt-36 text-white ml-[580px] ">        Signed in as {session.user.email} </p>
+<p className=" text-xl w-48 h-12 pt-36 text-white ml-[580px] ">        Welcome Mr. {session.user.name} </p>
+<img src={session.user.name} alt="/" className="rounded-full h-10 w-16" />
+        <button className="bg-blue w-48 h-12 text-4xl text-green-700 pt-40 ml-[580px]" onClick={() => signOut()}>Sign out</button>
+      </div>
     )
+  }
+  return (
+    <div className="w-full h-24 bg-black text-white">
+      <p className=" text-8xl w-[900px] h-12 pt-36 text-white ml-[420px] "> Not signed in ? </p>
+      <button className="bg-blue w-96 h-12 text-7xl text-green-700 pt-40 ml-[500px]" onClick={() => signIn()}>Sign in</button>
+    </div>
+  )
 }
-
-export default account
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,25 +1,18 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-
-export const authOptions = {
+import NextAuth from 'next-auth';
+// import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google'
+export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    // }),
     GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      }),
-      TwitterProvider({
-        clientId: process.env.TWITTER_CLIENT_ID,
-        clientSecret: process.env.TWITTER_CLIENT_SECRET
-      })
-    
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    }),
     // ...add more providers here
   ],
   secret: process.env.JWT_SECRET,
-}
-
-export default NextAuth(authOptions)
+});
