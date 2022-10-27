0de2acea36386188387b086059ef23efccd3ede9
Abhishek <abhishekparmar162@gmail.com>
2022-10-27 18:05:05 +0530

    added feed video

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Feed = () => {
   return (
-    <div className='basis-5/6'>
-      Feed
+    <div className='basis-5/6 '>
+     <iframe width="1260px" height="720px" src="https://www.youtube.com/embed/W3HulvsuqGI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   )
 }
